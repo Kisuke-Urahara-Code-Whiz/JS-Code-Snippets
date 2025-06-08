db049fb6b3d9f5f77b6e0650ee95a9cda5e365ca
Sougata Mondal <sougatacodewhiz@gmail.com>
2025-06-08 13:52:41 +0530

    Update app.js

--- Express-Starting-Code/app.js
+++ Express-Starting-Code/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
 const app = express();
-const methodOverride = require('method-override')
 
-app.set("view engine","ejs");
+var indexRouter = require('./routes/index');
+var authRouter = require('./routes/auth');
 
 const path = require("path");
 app.set("views",path.join(__dirname,"/views"));
@@ -11,8 +11,6 @@ app.set("views",path.join(__dirname,"/views"));
 app.use(express.static(path.join(__dirname,"public")));
 
 app.use(express.urlencoded({extended:true}));
-
-app.use(methodOverride('_method'));
 
 app.use(express.json());
 
